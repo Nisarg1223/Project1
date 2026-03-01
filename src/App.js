90c21a5bb9a63943c4e5c0753a77991b1234c4f3
Nisarg1223 <darjinisarg49@gmail.com>
2026-03-01 20:00:29 +0530

    frontend integrated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-
+const cors = require('cors');
 //require routes
 const AuthRouter = require('./routes/auth.route.js');
 const postRouter = require('./routes/post.route.js');
 const userRouter = require('./routes/user.routes.js');
 app.use(express.json())
 app.use(cookieParser())
-
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:5173'
+}));
 
 
 //using routes
